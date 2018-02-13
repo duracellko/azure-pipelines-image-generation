add23a5bbf3e6ed33297acb7fd12b394d41d5b7b
Rastislav Novotný <rasto@duracellko.net>
2018-02-13 22:59:54 +0100

    Install only Visual Studio 2017, Java 8 SDK, Android SDK and Azure CLI

--- images/win/scripts/Installers/Validate-JavaTools.ps1
+++ images/win/scripts/Installers/Validate-JavaTools.ps1
@@ -4,16 +4,13 @@
 ##  Desc:  Validate various JDKs and java tools
 ################################################################################
 
-if((Get-Command -Name 'java') -and (Get-Command -Name 'mvn') -and (Get-Command -Name 'ant') -and (Get-Command -Name 'gradle'))
+if(Get-Command -Name 'java')
 {
     Write-Host "Java $(java -version) on path"
-    Write-Host "Maven $(mvn -version) on path"
-    Write-Host "Ant $(ant -version) on path"
-    Write-Host "Gradle $(gradle -version) on path"
     exit 0
 }
 else
 {
-    Write-Host "one of Java,Maven,Ant,Gradle is not on path."
+    Write-Host "Java is not on path."
     exit 1
 }

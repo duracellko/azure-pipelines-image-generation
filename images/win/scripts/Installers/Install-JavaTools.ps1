b3f8f0df480c04de08ad5302e300055d9c396ee1
Rastislav Novotný <rasto@duracellko.net>
2018-06-21 18:45:57 +0200

    Install only Visual Studio 2017, Java 8 SDK and Android SDK

--- images/win/scripts/Installers/Install-JavaTools.ps1
+++ images/win/scripts/Installers/Install-JavaTools.ps1
@@ -7,13 +7,6 @@
 #Installing Both x86 and x64 versions of Java
 choco install jdk8 -params "both=true" -y
 
-choco install jdk9 -y
-choco install jdk10 -y
-choco install ant -y
-choco install cobertura -y
-choco install maven -y
-choco install gradle -y
-
 Import-Module -Name ImageHelpers -Force
 
 $currentPath = Get-MachinePath
@@ -32,12 +25,6 @@ foreach ($pathSegment in $pathSegments)
 $java8Installs = Get-ChildItem -Path 'C:\Program Files\Java' -Filter 'jdk*8*' | Sort-Object -Property Name -Descending | Select-Object -First 1
 $latestJava8Install = $java8Installs.FullName;
 
-$java9Installs = Get-ChildItem -Path 'C:\Program Files\Java' -Filter 'jdk*9*' | Sort-Object -Property Name -Descending | Select-Object -First 1
-$latestJava9Install = $java9Installs.FullName;
-
-$java10Installs = Get-ChildItem -Path 'C:\Program Files\Java' -Filter 'jdk*10*' | Sort-Object -Property Name -Descending | Select-Object -First 1
-$latestJava10Install = $java10Installs.FullName;
-
 $newPath = [string]::Join(';', $newPathSegments)
 $newPath = $latestJava8Install + '\bin;' + $newPath
 
@@ -45,21 +32,3 @@ Set-MachinePath -NewPath $newPath
 
 setx JAVA_HOME $latestJava8Install /M
 setx JAVA_HOME_8_X64 $latestJava8Install /M
-setx JAVA_HOME_9_X64 $latestJava9Install /M
-setx JAVA_HOME_10_X64 $latestJava10Install /M
-
-#Move maven variables to Machine, they may not be in the environment for this script so we need to read them from the registry.
-$userSid = (Get-WmiObject win32_useraccount -Filter "name = '$env:USERNAME' AND domain = '$env:USERDOMAIN'").SID
-$userEnvironmentKey = 'Registry::HKEY_USERS\' + $userSid + '\Environment'
-
-$m2_home = (Get-ItemProperty -Path $userEnvironmentKey -Name M2_HOME).M2_HOME
-$m2 = $m2_home + '\bin'
-$maven_opts = '-Xms256m'
-
-$m2_repo = 'C:\ProgramData\m2'
-New-Item -Path $m2_repo -ItemType Directory -Force
-
-setx M2 $m2 /M
-setx M2_HOME $m2_home /M
-setx M2_REPO $m2_repo /M
-setx MAVEN_OPTS $maven_opts /M
